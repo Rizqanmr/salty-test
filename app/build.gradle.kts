3b52e16329ddb58236925aaf41aff015b56bed2b
Rizqanmr <45690865+Rizqanmr@users.noreply.github.com>
2024-03-22 04:55:54 +0700

    create ui login

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,6 +62,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.material:material-icons-extended:1.6.4")
 
     // Ktor
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
---
+++ app/src/main/java/com/rizqanmr/saltytest/ui/screen/login/LoginActivity.kt
@@ -0,0 +1,231 @@
+package com.rizqanmr.saltytest.ui.screen.login
+
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusDirection
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.rizqanmr.saltytest.data.model.Credentials
+import com.rizqanmr.saltytest.ui.component.HeightSpacer
+import com.rizqanmr.saltytest.ui.component.WidthSpacer
+import com.rizqanmr.saltytest.ui.screen.main.MainActivity
+import com.rizqanmr.saltytest.ui.theme.SaltyTestTheme
+
+class LoginActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            SaltyTestTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    LoginForm()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun LoginForm() {
+    var credentials by remember { mutableStateOf(Credentials()) }
+    val context = LocalContext.current
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(horizontal = 30.dp)
+    ) {
+        EmailField(
+            value = credentials.email,
+            onChange = { data -> credentials = credentials.copy(email = data) },
+            modifier = Modifier.fillMaxWidth()
+        )
+        PasswordField(
+            value = credentials.password,
+            onChange = { data -> credentials = credentials.copy(password = data) },
+            submit = {
+                if (!checkCredentials(credentials, context)) credentials = Credentials()
+            },
+            modifier = Modifier.fillMaxWidth()
+        )
+        HeightSpacer(value = 10.dp)
+        LabeledCheckbox(
+            label = "Remember Me",
+            onCheckChanged = {
+                credentials = credentials.copy(remember = !credentials.remember)
+            },
+            isChecked = credentials.remember
+        )
+        HeightSpacer(value = 20.dp)
+        Button(
+            onClick = {
+                if (!checkCredentials(credentials, context)) credentials = Credentials()
+            },
+            enabled = credentials.isNotEmpty(),
+            shape = RoundedCornerShape(5.dp),
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text("Login")
+        }
+    }
+}
+
+@Composable
+fun EmailField(
+    value: String,
+    onChange: (String) -> Unit,
+    modifier: Modifier = Modifier,
+    label: String = "Email",
+    placeholder: String = "Enter your email"
+) {
+    val focusManager = LocalFocusManager.current
+    val leadingIcon = @Composable {
+        Icon(
+            Icons.Default.Person,
+            contentDescription = "",
+            tint = MaterialTheme.colorScheme.primary
+        )
+    }
+
+    TextField(
+        value = value,
+        onValueChange = onChange,
+        modifier = modifier,
+        leadingIcon = leadingIcon,
+        keyboardOptions = KeyboardOptions(imeAction = ImeAction.Next),
+        keyboardActions = KeyboardActions(
+            onNext = { focusManager.moveFocus(FocusDirection.Down) }
+        ),
+        placeholder = { Text(placeholder) },
+        label = { Text(label) },
+        singleLine = true,
+        visualTransformation = VisualTransformation.None
+    )
+}
+
+@Composable
+fun PasswordField(
+    value: String,
+    onChange: (String) -> Unit,
+    submit: () -> Unit,
+    modifier: Modifier = Modifier,
+    label: String = "Password",
+    placeholder: String = "Enter your Password"
+) {
+    var isPasswordVisible by remember { mutableStateOf(false) }
+
+    val leadingIcon = @Composable {
+        Icon(
+            Icons.Default.Lock,
+            contentDescription = "",
+            tint = MaterialTheme.colorScheme.primary
+        )
+    }
+    val trailingIcon = @Composable {
+        IconButton(onClick = { isPasswordVisible = !isPasswordVisible }) {
+            Icon(
+                if (isPasswordVisible) Icons.Default.VisibilityOff else Icons.Default.Visibility,
+                contentDescription = "",
+                tint = MaterialTheme.colorScheme.primary
+            )
+        }
+    }
+
+    TextField(
+        value = value,
+        onValueChange = onChange,
+        modifier = modifier,
+        leadingIcon = leadingIcon,
+        trailingIcon = trailingIcon,
+        keyboardOptions = KeyboardOptions(
+            imeAction = ImeAction.Done,
+            keyboardType = KeyboardType.Password
+        ),
+        keyboardActions = KeyboardActions(
+            onDone = { submit() }
+        ),
+        placeholder = { Text(placeholder) },
+        label = { Text(label) },
+        singleLine = true,
+        visualTransformation = if (isPasswordVisible) VisualTransformation.None else PasswordVisualTransformation()
+    )
+}
+
+@Composable
+fun LabeledCheckbox(
+    label: String,
+    onCheckChanged: () -> Unit,
+    isChecked: Boolean
+) {
+    Row(
+        Modifier
+            .clickable(
+                onClick = onCheckChanged
+            )
+            .padding(4.dp)
+    ) {
+        Checkbox(checked = isChecked, onCheckedChange = null)
+        WidthSpacer(value = 6.dp)
+        Text(label)
+    }
+}
+
+fun checkCredentials(creds: Credentials, context: Context): Boolean {
+    if (creds.isNotEmpty() && creds.email == "admin") {
+        context.startActivity(Intent(context, MainActivity::class.java))
+        return true
+    } else {
+        Toast.makeText(context, "Wrong Credentials", Toast.LENGTH_SHORT).show()
+        return false
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun LoginScreen() {
+    SaltyTestTheme {
+        LoginForm()
+    }
+}
