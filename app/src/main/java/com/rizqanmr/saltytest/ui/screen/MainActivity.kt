9bac848525bba8bc4de112ca61510784a9ad59ef
Rizqanmr <45690865+Rizqanmr@users.noreply.github.com>
2024-03-21 23:49:58 +0700

    Initial commit and setup dependency

---
+++ app/src/main/java/com/rizqanmr/saltytest/ui/screen/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.rizqanmr.saltytest.ui.screen
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.rizqanmr.saltytest.ui.theme.SaltyTestTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            SaltyTestTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    SaltyTestTheme {
+        Greeting("Android")
+    }
+}
